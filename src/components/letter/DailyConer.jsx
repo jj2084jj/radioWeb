d3028de0c812497aadb0a6d4eef52654894bb1a1
whdms3368 <whdms3368@nsmg21.com>
2021-12-17 13:50:52 +0900

    요일코너 변경

--- src/components/letter/DailyConer.jsx
+++ src/components/letter/DailyConer.jsx
@@ -1,3 +1,5 @@
+import "./DailyConer.scss";
+
 function DailyConer() {
   return (
     <>
@@ -7,11 +9,50 @@ function DailyConer() {
             <img src="https://jj2084jj.github.io/radioWeb/day.svg" alt="" />
             <h2>요일 코너</h2>
           </div>
-          <div>
+          <div className="dailyConerList">
             <ul>
-              <li>사연과 신청곡</li>
-              <li>하루 틈</li>
-              <li>내일 할 일</li>
+              <li>
+                <div className="dailyName">
+                  <h2> 월</h2>
+                </div>
+                <div>토마호크 배! 토막음악 퀴즈</div>
+              </li>
+              <li>
+                <div className="dailyName">
+                  <h2> 화</h2>
+                </div>
+                <div>영업합니다 시즌2</div>
+              </li>
+              <li>
+                <div className="dailyName">
+                  <h2> 수</h2>
+                </div>
+                <div>ㄱ이나니...?</div>
+              </li>
+              <li>
+                <div className="dailyName">
+                  <h2> 목</h2>
+                </div>
+                <div>아무노래</div>
+              </li>
+              <li>
+                <div className="dailyName">
+                  <h2> 금</h2>
+                </div>
+                <div>깨끗하고 어두운 곳</div>
+              </li>
+              <li>
+                <div className="dailyName">
+                  <h2> 토</h2>
+                </div>
+                <div>우리 삶이 음악이 된다면</div>
+              </li>
+              <li>
+                <div className="dailyName">
+                  <h2> 일</h2>
+                </div>
+                <div>고전이 빛나는 밤</div>
+              </li>
             </ul>
           </div>
         </div>
